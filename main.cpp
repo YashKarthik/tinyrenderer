de41ab7f1af508d2e2fcc61a37129cb680209988
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-11-24 16:38:37 -0500

    drew lines diff ways; finally used y = mx + c irl

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "tgaimage.h"
+#include <fstream>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -7,37 +8,34 @@ const TGAColor blue = TGAColor(37, 164, 249, 1);
 const TGAColor yellow = TGAColor(253, 190, 0, 255);
 const TGAColor black = TGAColor(0, 0, 0, 255);
 
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
+
 int main() {
   TGAImage image(100, 100, TGAImage::RGB);
 
+  image.set(50, 10, white);
+  image.set(50, 12, blue);
+
+  // works
+  line(10, 10, 30, 30, image, white);
+  line(30, 30, 10, 10, image, red);
+
+  line(10, 20, 80, 30, image, white);
+  line(10, 10, 80, 30, image, blue);
+
   image.flip_vertically();
-
-  for (std::size_t i{0}; i < 10; i++) {
-    for (std::size_t j{0}; j < 10; j++) {
-      image.set(10+j, 10+i, red);
-    }
-  }
-  for (std::size_t i{0}; i < 10; i++) {
-    for (std::size_t j{0}; j < 10; j++) {
-      image.set(21+j, 10+i, green);
-    }
-  }
-  for (std::size_t i{0}; i < 10; i++) {
-    for (std::size_t j{0}; j < 10; j++) {
-      image.set(10+j, 21+i, blue);
-    }
-  }
-  for (std::size_t i{0}; i < 10; i++) {
-    for (std::size_t j{0}; j < 10; j++) {
-      image.set(21+j, 10+i, green);
-    }
-  }
-  for (std::size_t i{0}; i < 10; i++) {
-    for (std::size_t j{0}; j < 10; j++) {
-      image.set(21+j, 21+i, yellow);
-    }
-  }
-
   image.write_tga_file("output.tga");
   return 0;
+}
+
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+  if (x0 > x1) {
+    std::swap(x0, x1);
+    std::swap(y0, y1);
+  }
+
+  for (int x{x0}; x < x1; x++) {
+    int y = y0 + (x - x0)*(y1 - y0)/(float)(x1 - x0);
+    image.set(x, y, color);
+  }
 }
