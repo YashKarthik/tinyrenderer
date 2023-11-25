ea0d8a618058a99cc69f9b54bd37d7d297dce55e
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-11-24 23:30:26 -0500

    wireframe!!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Building a 3D renderer from scratch.
+
+https://github.com/ssloy/tinyrenderer/wiki/
--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
-#include "tgaimage.h"
 #include <fstream>
+#include <cmath>
+#include <vector>
+#include "model.h"
+#include "geometry.h"
+#include "tgaimage.h"
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
@@ -8,21 +12,37 @@ const TGAColor blue = TGAColor(37, 164, 249, 1);
 const TGAColor yellow = TGAColor(253, 190, 0, 255);
 const TGAColor black = TGAColor(0, 0, 0, 255);
 
+const int width = 800;
+const int height = 800;
+
+Model *model = NULL;
+
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color);
 
-int main() {
-  TGAImage image(100, 100, TGAImage::RGB);
+int main(int argc, char** argv) {
+  if (argc == 2) {
+    model = new Model(argv[1]);
+  } else {
+    model = new Model("./obj/african_head.obj");
+  }
 
-  image.set(50, 10, white);
-  image.set(50, 12, blue);
+  TGAImage image(width, height, TGAImage::RGB);
+  for (int i{0}; i < model->nfaces(); i++) {
 
-  line(10, 10, 30, 30, image, white);
-  line(30, 30, 10, 10, image, red);
+    std::vector<int> face = model->face(i);
+    for (int j{0}; j < 3; j++) {
 
-  line(10, 20, 80, 30, image, white);
-  line(10, 10, 80, 30, image, blue);
+      Vec3f v0 = model->vert(face[j]);
+      Vec3f v1 = model->vert(face[(j + 1) % 3]);
 
-  line(40, 5, 50, 90, image, yellow);
+      int x0 = (v0.x + 1.0)*width/2.0;
+      int y0 = (v0.y + 1.0)*width/2.0;
+      int x1 = (v1.x + 1.0)*width/2.0;
+      int y1 = (v1.y + 1.0)*width/2.0;
+
+      line(x0, y0, x1, y1, image, white);
+    }
+  }
 
   image.flip_vertically();
   image.write_tga_file("output.tga");
@@ -42,8 +62,9 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     std::swap(y0, y1);
   }
 
+  float m = (y1 - y0)/(float)(x1 - x0);
   for (int x{x0}; x < x1; x++) {
-    int y = y0 + (x - x0)*(y1 - y0)/(float)(x1 - x0); // y = y0 + m*Dx
+    int y = y0 + (x - x0)*m; // y = y0 + m*Dx
 
     if (steep) image.set(y, x, color);
     else image.set(x, y, color);
